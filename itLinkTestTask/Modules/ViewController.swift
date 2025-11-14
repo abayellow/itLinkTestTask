0e2a5ae49e222094a394392cda027ef9342e0796
abayellow <sasha.abanshin@gmail.com>
2025-11-14 17:58:56 +1100

    Add folders

---
+++ itLinkTestTask/Modules/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  itLinkTestTask
+//
+//  Created by Alexander Abanshin on 14.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
